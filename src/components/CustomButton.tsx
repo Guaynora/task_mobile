cf5e11a1f29c3f27a737b9fed334e974452c18a4
Jonathan Guaynora <guaynorajon@gmail.com>
2023-05-24 21:18:58 -0500

    custom button component

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const CustomButton = ({text, onPress}: {text: string; onPress: () => void}) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#C2DDFC',
+    height: 50,
+    width: 250,
+    justifyContent: 'center',
+    borderRadius: 12,
+    marginVertical: 10,
+  },
+  text: {
+    color: '#000',
+    textAlign: 'center',
+  },
+});
+
+export default CustomButton;
